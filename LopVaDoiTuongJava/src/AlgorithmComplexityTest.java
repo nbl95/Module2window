162ee612c47769df2ee85a2ecd45a82815a8afde
nbl95 <nbl.ltu12@gmail.com>
2020-01-16 15:06:24 +0700

    abc

---
+++ LopVaDoiTuongJava/src/AlgorithmComplexityTest.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class AlgorithmComplexityTest {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.printf("Enter a string");
+        String inputString = scanner.nextLine();
+
+        int[] frequentChar = new int[255];
+        for(int i = 0; i <inputString.length(); i++){
+            int ascii = (int) inputString.charAt(i);
+
+            frequentChar[ascii]+=1;
+
+            int max = 0;
+            char character = (char) 225;
+            for (int j =0; j<255; j++){
+                if(frequentChar[j]> max){
+                    character= (char) j;
+                }
+            }
+            System.out.printf("The most appearing letter is '" + character + "' with a frequency of " + max + " times");
+        }
+    }
+}
